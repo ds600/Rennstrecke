091405687c22fa02ec2737fa2fb3f48e3b391d13
ds600 <ageofdavid1@outlook.com>
2020-08-15 14:36:27 +0200

    FIrst commit

---
+++ Assets/Scripts/Fahrzeug.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fahrzeug : MonoBehaviour
+{
+    float drehmomentFaktor = 25000;
+    public WheelCollider collVL, collVR, collHL, collHR;
+
+    float lenkwinkelFaktor = 30;
+    public GameObject radVL, radVR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float drehmoment = drehmomentFaktor * Time.deltaTime * Input.GetAxis("Vertical");
+        collVL.motorTorque = drehmoment;
+        collVR.motorTorque = drehmoment;
+        collHL.motorTorque = drehmoment;
+        collHR.motorTorque = drehmoment;
+
+        float lenkwinkel = lenkwinkelFaktor * Input.GetAxis("Horizontal");
+        collVL.steerAngle = lenkwinkel;
+        collVR.steerAngle = lenkwinkel;
+
+        radVL.transform.localEulerAngles = new Vector3(radVL.transform.localEulerAngles.x, collVL.steerAngle, radVL.transform.localEulerAngles.z);
+        radVR.transform.localEulerAngles = new Vector3(radVR.transform.localEulerAngles.x, collVR.steerAngle, radVR.transform.localEulerAngles.z);
+    }
+}
---
+++ Assets/Scripts/KameraBewegen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraBewegen : MonoBehaviour
+{
+    public GameObject fahrzeug;
+    float abstandXZ = 6;
+    float hoeheY = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Richtung in die das Auto schaut
+        Quaternion fahrzeugRoationY = new Quaternion();
+        fahrzeugRoationY.eulerAngles = new Vector3(0, fahrzeug.transform.eulerAngles.y, 0);
+
+        // Entfernung hinter das Auto
+        Vector3 abstandHinterFahrzeug = fahrzeugRoationY * new Vector3(0, 0, abstandXZ);
+        
+        // Bewegt die Kamera hinter das Auto in höhe der Entfernung
+        transform.position = fahrzeug.transform.position - abstandHinterFahrzeug;
+
+        // Die Kamera auf die Richtige höhe stellen
+        transform.position = new Vector3(transform.position.x, transform.position.y + hoeheY, transform.position.z);
+
+        // Rotation immer so richten, dass sie auf das Auto guckt
+        transform.LookAt(fahrzeug.transform);
+
+
+    }
+}
